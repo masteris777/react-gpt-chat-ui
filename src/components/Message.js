226ea3a27e31fac7304d9edeb4b8b045a269ab24
masteris777 <masteris@gmail.com>
2023-05-28 15:34:44 +0300

    styling change

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,7 +4,7 @@ import ReactMarkdown from "react-markdown";
 // This component will render a single message
 export function Message({ message }) {
 	return (
-		<tr>
+		<tr className={`message-row ${message.model ? "model" : "user"}`}>
 			<td className="senderCell">
 				{message.model ? message.model : message.sender}
 			</td>
